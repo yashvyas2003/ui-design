e90be681a7bb30e4dcb5d476b9fd906a49a7b455
yashvyas2003 <vyasy3083@gmail.com>
2025-03-13 12:16:32 +0530

    Add files via upload

---
+++ lab4/bin/lab4.dart
@@ -0,0 +1,8 @@
+import 'package:lab4/lab4.dart';
+import 'dart:io';
+void main(List<String> arguments) {
+          var x = stdin.readLineSync()!;
+          lab4 lb = lab4();
+          print(lb.sname(x));
+}
+
---
+++ lab4/lib/lab4.dart
@@ -0,0 +1,12 @@
+class lab4{
+          String sname(String color){
+                 return (switch(color){
+                      ('red'||'Red'||'RED') => 'danger',
+                      ('green'||'Green'||'GREEN') => 'another',
+                      ('black'||'Black'||'BLACK') => 'other',
+
+            _=>'not listed ',
+});
+ 
+}
+}
