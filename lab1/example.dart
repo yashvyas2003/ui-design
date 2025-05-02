a5ad04d1a639ac4c05f5d65c2aa05baf70420948
yashvyas2003 <vyasy3083@gmail.com>
2025-05-02 21:04:54 +0530

    Add files via upload

---
+++ lab1/example.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+void main(){
+print("ACE");
+print("College");   
+stdout.write("ACE College");
+print("enter name of dept:");
+String dept = stdin.readLineSync()!;
+//String str =null;
+print(dept);
+}
---
+++ lab1/sample.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+void main(){
+  int a = int.parse(stdin.readLineSync()!);
+	int b = int.parse(stdin.readLineSync()!);
+  print("The sum of $a and $b is ${a+b}");	
+  
+  }
